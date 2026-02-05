d631f32e39b99b7f468defe33988a401508ea8ba
Maintenance-Domagne <coster@eclorsa.com>
2026-02-05 13:49:49 +0100

    ok

--- mysql/create_db.sql
+++ mysql/create_db.sql
@@ -1,12 +1,19 @@
-CREATE DATABASE IF NOT EXISTS maintenance_ai;
+IF DB_ID('maintenance_ai') IS NULL
+BEGIN
+    CREATE DATABASE maintenance_ai;
+END
+GO
+
 USE maintenance_ai;
+GO
 
 CREATE TABLE pannes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT IDENTITY(1,1) PRIMARY KEY,
     machine VARCHAR(50) NOT NULL,
     category VARCHAR(50) NOT NULL,
-    description TEXT NOT NULL,
-    keywords TEXT NOT NULL,
-    solutions TEXT NOT NULL,
+    description NVARCHAR(MAX) NOT NULL,
+    keywords NVARCHAR(MAX) NOT NULL,  -- mots-clés séparés par des virgules
+    solutions NVARCHAR(MAX) NOT NULL, -- solutions séparées par "|"
     priority INT DEFAULT 1
 );
+GO
